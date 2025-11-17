101cb1a4b8abeecc594bbb425cb50b227d73a9ed
ashmikan <ashmika.nathali123@gmail.com>
2025-11-17 22:46:12 +0530

    create background

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import WeatherBackground from './components/WeatherBackground'
 
 const App = () => {
+  const [weather, setWeather] = React.useState(null);
+
+  const getWeatherCondition = () => weather && ({
+    main: weather.weather[0].main,
+    isDay : Date.now() / 1000 >= weather.sys.sunrise && Date.now() / 1000 < weather.sys.sunset
+  })
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className='min-h-screen'>
+      <WeatherBackground condition={getWeatherCondition()} />
+
+      <div className='flex items-center justify-center p-6 min-h-screen'>
+        <div className='bg-transparent backdrop-filter backdrop-blur-md rounded-xl shadow-2xl rounded-xl p-8 max-w-md text-white w-full border-white/30 relative z-10'>
+          <h1 className='text-4xl font-extrabold text-center mb-6'>Weather App</h1>
+        </div>
+      </div>
     </div>
   )
 }
